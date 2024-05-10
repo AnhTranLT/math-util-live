20762e826ff85788afe3c61c224d95105440f9fb
AnhTranLT <anhtranlt03@gmail.com>
2024-05-10 20:08:18 +0700

    fix first

--- src/com/fu/mathutil/MathUtilLive.java
+++ src/com/fu/mathutil/MathUtilLive.java
@@ -19,6 +19,8 @@ public class MathUtilLive {
         System.out.println("expected: 5! = 120; actual: "  + result);
         
         System.out.println("expected: 6! = 720; actual: " +MathUtil.getFactorial(6));
-        
+     // thêm code sau khi up len server 8:04 10/5/2024
+        System.out.println("expected: 1! = 1; actual: " +MathUtil.getFactorial(1));
+        System.out.println("expected: 3! = 6; actual: " +MathUtil.getFactorial(3));
     }  
 }
